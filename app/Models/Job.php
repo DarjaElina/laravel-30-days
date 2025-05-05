608716d2b45d9a07523d1b60b4d058cc0f4ab506
DarjaElina <daria.elina98@gmail.com>
2025-05-05 22:47:51 +0300

    add days 1-7

---
+++ app/Models/Job.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Models;
+use \Illuminate\Support\Arr;
+
+class Job {
+  public static function all(): array {
+    return [
+        [
+            'title' => 'Director',
+            'salary' => '$50,000',
+            'id' => 1
+        ],
+        [
+            'title' => 'Programmer',
+            'salary' => '$10,000',
+            'id' => 2
+        ],
+        [
+            'title' => 'Teacher',
+            'salary' => '$40,000',
+            'id' => 3
+        ]
+    ];
+  }
+
+  public static function find(int $id): array {
+    $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+    if (!$job) {
+      abort(404);
+    }
+
+    return $job;
+  }
+}
