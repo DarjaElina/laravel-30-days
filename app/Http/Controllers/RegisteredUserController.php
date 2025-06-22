43d7f4766d44170880eae229d5848a29d487bfd0
DarjaElina <daria.elina98@gmail.com>
2025-06-23 02:27:17 +0300

    day 22: add authentication logic

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -12,6 +15,22 @@ class RegisteredUserController extends Controller
     }
     public function store()
     {
-        dd('todo!');
+        // validate
+        $validated = request()->validate([
+          'first_name' =>  ['required'],
+          'last_name' =>  ['required'],
+          'email' =>  ['required', 'email'],
+          'password' =>  ['required', Password::min(6), 'confirmed'],
+        ]);
+
+
+        // create the user
+        $user = User::create($validated);
+
+        // log in
+        Auth::login($user);
+
+        // redirect somewhere
+        return redirect('/jobs');
     }
 }
