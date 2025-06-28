0070ed28be7dc501279faf9b20393a79693fcc3d
DarjaElina <daria.elina98@gmail.com>
2025-06-29 02:08:05 +0300

    day 26: intro to vite

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite(['resources/js/app.js'])
 
         <title>Home Page</title>
     </head>
-    <script src="https://cdn.tailwindcss.com"></script>
     <body class="h-full">
   <div class="min-h-full">
       <nav class="bg-gray-800">
